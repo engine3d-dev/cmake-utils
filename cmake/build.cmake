ee849902cd1f9300424d34ce7f79e829fe121f11
= <=>
2024-11-12 17:25:30 -0800

    Added build cmake for generating the demos executable

--- cmake/build.cmake
+++ cmake/build.cmake
@@ -2,34 +2,135 @@
 # set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE INTERNAL "") # works (in creating the compile_commands.json file)
 
-# Copy to source directory
-add_custom_target(
-    copy-compile-commands ALL
-    DEPENDS
-        ${CMAKE_SOURCE_DIR}/compile_commands.json
-)
+# Define the function that accepts a variable number of arguments
+# used for building applications
+function(build_demos)
+  # Parse CMake function arguments
+  set(options)
+  set(one_value_args)
+  set(multi_value_args SOURCES INCLUDES PACKAGES LINK_LIBRARIES)
+  cmake_parse_arguments(DEMOS_ARGS
+    "${options}"
+    "${one_value_args}"
+    "${multi_value_args}"
+    ${ARGN})
 
-add_custom_command(
-    OUTPUT ${CMAKE_CURRENT_LIST_DIR}/compile_commands.json
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different
-        ${CMAKE_BINARY_DIR}/compile_commands.json
+    set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE INTERNAL "") # works (in creating the compile_commands.json file)
+
+    add_executable(${PROJECT_NAME} ${DEMOS_ARGS_SOURCES})
+    # Copy to compile_commands.json for .clangd
+    add_custom_target(
+        copy-compile-commands ALL
+        DEPENDS
+            ${CMAKE_SOURCE_DIR}/compile_commands.json
+    )
+
+    add_custom_command(
+        OUTPUT ${CMAKE_CURRENT_LIST_DIR}/compile_commands.json
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            ${CMAKE_BINARY_DIR}/compile_commands.json
         ${CMAKE_CURRENT_LIST_DIR}/compile_commands.json
-    DEPENDS
-    # Unlike "proper" targets like executables and libraries, 
-    # custom command / target pairs will not set up source
-    # file dependencies, so we need to list file explicitly here
-    generate-compile-commands
-    ${CMAKE_BINARY_DIR}/compile_commands.json
-)
-
-# Generate the compilation commands. Necessary so cmake knows where it came
-# from and if for some reason you delete it.
-add_custom_target(generate-compile-commands
-    DEPENDS
+        DEPENDS
+        # Unlike "proper" targets like executables and libraries, 
+        # custom command / target pairs will not set up source
+        # file dependencies, so we need to list file explicitly here
+        generate-compile-commands
         ${CMAKE_BINARY_DIR}/compile_commands.json
-)
+    )
 
-add_custom_command(
-    OUTPUT ${CMAKE_BINARY_DIR}/compile_commands.json
-    COMMAND ${CMAKE_COMMAND} -B${CMAKE_BINARY_DIR} -S${CMAKE_SOURCE_DIR}
-)
+    # Generate the compilation commands. Necessary so cmake knows where it came
+    # from and if for some reason you delete it.
+    add_custom_target(generate-compile-commands
+        DEPENDS
+            ${CMAKE_BINARY_DIR}/compile_commands.json
+    )
+
+    add_custom_command(
+        OUTPUT ${CMAKE_BINARY_DIR}/compile_commands.json
+        COMMAND ${CMAKE_COMMAND} -B${CMAKE_BINARY_DIR} -S${CMAKE_SOURCE_DIR}
+    )
+
+    # This is used because if we do not have this users systems may give them a linked error with oldnames.lib
+    # Usage - used to suppress that lld-link error and use the defaulted linked .library
+    if(MSVC)
+        target_compile_options(${PROJECT_NAME} PUBLIC "/Z1" "/NOD")
+    endif(MSVC)
+    
+    find_package(OpenGL REQUIRED)
+    find_package(glfw3 REQUIRED)
+
+    find_package(Vulkan REQUIRED)
+    find_package(VulkanHeaders REQUIRED)
+
+    if(LINUX)
+    find_package(VulkanLoader REQUIRED)
+    endif(LINUX)
+
+
+    # Including
+    target_include_directories(${PROJECT_NAME} PUBLIC ./)
+
+    find_package(glm REQUIRED)
+    find_package(fmt REQUIRED)
+    find_package(spdlog REQUIRED)
+    find_package(yaml-cpp REQUIRED)
+    find_package(imguidocking REQUIRED)
+    find_package(box2d REQUIRED)
+    find_package(joltphysics REQUIRED)
+    find_package(EnTT REQUIRED)
+
+    if(WIN32)
+    target_link_libraries(
+        ${PROJECT_NAME}
+        PRIVATE
+        glfw
+        ${OPENGL_LIBRARIES}
+        Vulkan::Vulkan
+        vulkan-headers::vulkan-headers
+        glm::glm
+        fmt::fmt
+        spdlog::spdlog
+        yaml-cpp::yaml-cpp
+        imguidocking::imguidocking
+        box2d::box2d
+        Jolt::Jolt
+        EnTT::EnTT
+    )
+    endif(WIN32)
+
+    if(LINUX)
+    target_link_libraries(
+        ${PROJECT_NAME}
+        PRIVATE
+        glfw
+        ${OPENGL_LIBRARIES}
+        Vulkan::Loader
+        glm::glm
+        fmt::fmt
+        spdlog::spdlog
+        yaml-cpp::yaml-cpp
+        imguidocking::imguidocking
+        box2d::box2d
+        Jolt::Jolt
+        EnTT::EnTT
+    )
+    endif(LINUX)
+
+    if(APPLE)
+    target_link_libraries(
+        ${PROJECT_NAME}
+        PRIVATE
+        glfw
+        ${OPENGL_LIBRARIES}
+        Vulkan::Vulkan
+        # vulkan-headers::vulkan-headers
+        glm::glm
+        fmt::fmt
+        spdlog::spdlog
+        yaml-cpp::yaml-cpp
+        imguidocking::imguidocking
+        box2d::box2d
+        EnTT::EnTT
+    )
+    endif(APPLE)
+endfunction()
---
+++ conanfile.py
@@ -0,0 +1,52 @@
+from conan import ConanFile
+from conan.tools.files import copy
+from conan.tools.layout import basic_layout
+import os
+
+
+required_conan_version = ">=2.0.6"
+
+
+class engine3d_cmake_util_conan(ConanFile):
+    name = "engine3d-cmake-utils"
+    version = "1.0"
+    license = "Apache-2.0"
+    description = ("A collection of CMake for engine3d")
+    topics = ("cmake", "engine3d")
+    exports_sources = ("cmake/*", "LICENSE")
+    no_copy_source = True
+    options = {
+        "add_build_outputs": [True, False],
+        "optimize_debug_build": [True, False]
+    }
+    default_options = {
+        "add_build_outputs": True,
+        "optimize_debug_build": True
+    }
+
+    def package_id(self):
+        self.info.clear()
+
+    def layout(self):
+        basic_layout(self)
+
+    def package(self):
+        copy(self, "LICENSE", dst=os.path.join(
+            self.package_folder, "licenses"),  src=self.source_folder)
+        copy(self, "cmake/*.cmake", src=self.source_folder,
+             dst=self.package_folder)
+        copy(self, "cmake/*.conf", src=self.source_folder,
+             dst=self.package_folder)
+        
+    
+    def package_info(self):
+        # Add toolchain.cmake to user_toolchain configuration info to be used
+        # by CMakeToolchain generator
+        build_path = os.path.join(
+            self.package_folder, "cmake/build.cmake")
+
+        self.conf_info.append(
+            "tools.cmake.cmaketoolchain:user_toolchain",
+            build_path)
+
+
