791320c0617119d1e5840b0e71907794e883fbd8
= <=>
2024-11-12 17:36:56 -0800

    Added library cmake function

--- cmake/build.cmake
+++ cmake/build.cmake
@@ -2,22 +2,7 @@
 # set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE INTERNAL "") # works (in creating the compile_commands.json file)
 
-# Define the function that accepts a variable number of arguments
-# used for building applications
-function(build_demos)
-  # Parse CMake function arguments
-  set(options)
-  set(one_value_args)
-  set(multi_value_args SOURCES INCLUDES PACKAGES LINK_LIBRARIES)
-  cmake_parse_arguments(DEMOS_ARGS
-    "${options}"
-    "${one_value_args}"
-    "${multi_value_args}"
-    ${ARGN})
-
-    set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE INTERNAL "") # works (in creating the compile_commands.json file)
-
-    add_executable(${PROJECT_NAME} ${DEMOS_ARGS_SOURCES})
+function(generate_compile_commands)
     # Copy to compile_commands.json for .clangd
     add_custom_target(
         copy-compile-commands ALL
@@ -49,6 +34,24 @@ function(build_demos)
         OUTPUT ${CMAKE_BINARY_DIR}/compile_commands.json
         COMMAND ${CMAKE_COMMAND} -B${CMAKE_BINARY_DIR} -S${CMAKE_SOURCE_DIR}
     )
+endfunction()
+
+# Define the function that accepts a variable number of arguments
+# used for building applications
+function(build_demos)
+  # Parse CMake function arguments
+  set(options)
+  set(one_value_args)
+  set(multi_value_args SOURCES INCLUDES PACKAGES LINK_LIBRARIES)
+  cmake_parse_arguments(DEMOS_ARGS
+    "${options}"
+    "${one_value_args}"
+    "${multi_value_args}"
+    ${ARGN})
+
+    set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE INTERNAL "") # works (in creating the compile_commands.json file)
+
+    add_executable(${PROJECT_NAME} ${DEMOS_ARGS_SOURCES})
 
     # This is used because if we do not have this users systems may give them a linked error with oldnames.lib
     # Usage - used to suppress that lld-link error and use the defaulted linked .library
@@ -66,6 +69,7 @@ function(build_demos)
     find_package(VulkanLoader REQUIRED)
     endif(LINUX)
 
+    generate_compile_commands()
 
     # Including
     target_include_directories(${PROJECT_NAME} PUBLIC ./)
@@ -134,3 +138,105 @@ function(build_demos)
     )
     endif(APPLE)
 endfunction()
+
+
+function(build_library)
+  # Parse CMake function arguments
+  set(options)
+  set(one_value_args)
+  set(multi_value_args SOURCES INCLUDES PACKAGES LINK_LIBRARIES)
+  cmake_parse_arguments(DEMOS_ARGS
+    "${options}"
+    "${one_value_args}"
+    "${multi_value_args}"
+    ${ARGN})
+
+    set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE INTERNAL "") # works (in creating the compile_commands.json file)
+
+    add_library(${PROJECT_NAME} ${DEMOS_ARGS_SOURCES})
+
+    # This is used because if we do not have this users systems may give them a linked error with oldnames.lib
+    # Usage - used to suppress that lld-link error and use the defaulted linked .library
+    if(MSVC)
+        target_compile_options(${PROJECT_NAME} PUBLIC "/Z1" "/NOD")
+    endif(MSVC)
+    
+    find_package(OpenGL REQUIRED)
+    find_package(glfw3 REQUIRED)
+
+    find_package(Vulkan REQUIRED)
+    find_package(VulkanHeaders REQUIRED)
+
+    if(LINUX)
+    find_package(VulkanLoader REQUIRED)
+    endif(LINUX)
+
+    generate_compile_commands()
+
+    target_include_directories(${PROJECT_NAME} PUBLIC ./)
+
+    find_package(glm REQUIRED)
+    find_package(fmt REQUIRED)
+    find_package(spdlog REQUIRED)
+    find_package(yaml-cpp REQUIRED)
+    find_package(imguidocking REQUIRED)
+    find_package(box2d REQUIRED)
+    find_package(joltphysics REQUIRED)
+    find_package(EnTT REQUIRED)
+
+    if(WIN32)
+    target_link_libraries(
+        ${PROJECT_NAME}
+        PRIVATE
+        glfw
+        ${OPENGL_LIBRARIES}
+        Vulkan::Vulkan
+        vulkan-headers::vulkan-headers
+        glm::glm
+        fmt::fmt
+        spdlog::spdlog
+        yaml-cpp::yaml-cpp
+        imguidocking::imguidocking
+        box2d::box2d
+        Jolt::Jolt
+        EnTT::EnTT
+    )
+    endif(WIN32)
+
+    if(LINUX)
+    target_link_libraries(
+        ${PROJECT_NAME}
+        PRIVATE
+        glfw
+        ${OPENGL_LIBRARIES}
+        Vulkan::Loader
+        glm::glm
+        fmt::fmt
+        spdlog::spdlog
+        yaml-cpp::yaml-cpp
+        imguidocking::imguidocking
+        box2d::box2d
+        Jolt::Jolt
+        EnTT::EnTT
+    )
+    endif(LINUX)
+
+    if(APPLE)
+    target_link_libraries(
+        ${PROJECT_NAME}
+        PRIVATE
+        glfw
+        ${OPENGL_LIBRARIES}
+        Vulkan::Vulkan
+        # vulkan-headers::vulkan-headers
+        glm::glm
+        fmt::fmt
+        spdlog::spdlog
+        yaml-cpp::yaml-cpp
+        imguidocking::imguidocking
+        box2d::box2d
+        EnTT::EnTT
+    )
+    endif(APPLE)
+endfunction()
+
